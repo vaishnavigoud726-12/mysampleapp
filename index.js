2ac4ee26687d6d79b20fd8c9def26d3444bb0b25
vaishnavigoud726-12 <vaishnavigoud726@gmail.com>
2026-09-18 10:11:01 +0530

    second commit

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 let express=require('express');
+
+
+let mongoose=require('mongoose');
 let hrroutes=require('./routes/hr_routes');
-
+let emproutes=require('./routes/emp_routes');
 let app=express();
-app.use("/api/hr",hrroutes);
+mongoose.connect("mongodb://localhost:27017/hrmanagement").then(
+    ()=>console.log("Database connected successfully")   
+).catch((err)=>console.log(err));
 
+app.use(express.json());
+app.use("/api/hr",hrroutes);
+app.use("/api/emp",emproutes);
 //run the server in port 3000
 app.listen(3000,()=>{
     console.log("server is running on port 3000")
---
+++ routes/emp_routes.js
@@ -0,0 +1,20 @@
+let express=require('express');
+let router=express.Router();
+
+router.post("/register",(req,res)=>{
+    let data=req.body;
+    res.send(data.name);
+})
+router.post("/login",(req,res)=>{
+    res.send("login page called");
+})
+router.get("/viewtasks",(req,res)=>{
+    res.send("view task page called");
+})
+router.get("/viewtodo",(req,res)=>{
+    res.send("view todo page called");
+})
+router.put("/updateprofile",(req,res)=>{
+    res.send("update profile page called");
+})
+module.exports=router;
